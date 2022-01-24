ee98da2638ffd1392647c8c90683af56938318d2
Aniketh Hongalgi <aniketh@live.unc.edu>
2022-01-24 16:45:14 -0500

    VariableChange

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -4,60 +4,60 @@ __author__ = "730243145"
 
 # instantiate variables that accept user input
 
-word: str = input("Enter a 5-character word:")
+WORD: str = input("Enter a 5-character word:")
 
 # Checking that user input abides by set constraints
 
-if (len(word) == 5):
-    character: str = input("Enter a single character:")
-    if (len(character) == 1):
-        print("Searching for " + character + " in " + word)
+if (len(WORD) == 5):
+    CHARACTER: str = input("Enter a single character:")
+    if (len(CHARACTER) == 1):
+        print("Searching for " + CHARACTER + " in " + WORD)
 
         # Number of occurances 
-        
-        count = 0
 
-# Searching string for inputed character acorss indexes
+        COUNT = 0
 
-        if (word[0] == character):
-            print(character + " found at index 0")
-            count = count + 1
+# Searching string for inputed CHARACTER acorss indexes
 
-        if (word[1] == character):
-            print(character + " found at index 1")
-            count = count + 1
+        if (WORD[0] == CHARACTER):
+            print(CHARACTER + " found at index 0")
+            COUNT = COUNT + 1
 
-        if (word[2] == character):
-            print(character + " found at index 2")
-            count = count + 1
+        if (WORD[1] == CHARACTER):
+            print(CHARACTER + " found at index 1")
+            COUNT = COUNT + 1
 
-        if (word[3] == character):
-            print(character + " found at index 3")
-            count = count + 1
+        if (WORD[2] == CHARACTER):
+            print(CHARACTER + " found at index 2")
+            COUNT = COUNT + 1
 
-        if (word[4] == character):
-            print(character + " found at index 4")
-            count = count + 1
+        if (WORD[3] == CHARACTER):
+            print(CHARACTER + " found at index 3")
+            COUNT = COUNT + 1
 
-        if count == 0: 
-            print("No instances of" + character + " in " + word)
-        if count > 1:
-            print(str(count) + " instances of " + character + " found in " + word)
+        if (WORD[4] == CHARACTER):
+            print(CHARACTER + " found at index 4")
+            COUNT = COUNT + 1
+
+        if COUNT == 0: 
+            print("No instances of" + CHARACTER + " in " + WORD)
+        if COUNT > 1:
+            print(str(COUNT) + " instances of " + CHARACTER + " found in " + WORD)
 
 # Error messages if constraints not met
 
-        if count < 1:
-            print("No instances of " + character + " found in " + word)
+        if COUNT < 1:
+            print("No instances of " + CHARACTER + " found in " + WORD)
         else:
 
 
-            print(str(count) + " instance of " + character + " found in " + word)
+            print(str(COUNT) + " instance of " + CHARACTER + " found in " + WORD)
 
 
     else: 
-        print("Error: Character must be a single character")
+        print("Error: Character must be a single Character")
         quit()
 
 else:
-    print("Error: Word must contain 5 characters")
+    print("Error: WORD must contain 5 characters")
     quit()
