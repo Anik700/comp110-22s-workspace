39a70c117ab0dbe113880829b2d75d40cfa6fc04
Aniketh Hongalgi <aniketh@live.unc.edu>
2022-01-24 16:12:22 -0500

    commit

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,38 +2,49 @@
 
 __author__ = "730243145"
 
+# instantiate variables that accept user input
 word: str = input("Enter a 5-character word:")
+# Checking that user input abides by set constraints
 if (len(word) == 5):
     character: str = input("Enter a single character:")
     if (len(character) == 1):
         print("Searching for " + character + " in " + word)
+        # Number of occurances 
         count = 0
-
+# Searching string for inputed character acorss indexes
         if (word[0] == character):
             print(character + " found at index 0")
-            count =+ 1
+            count = count + 1
 
         if (word[1] == character):
             print(character + " found at index 1")
-            count =+ 1
+            count = count + 1
 
         if (word[2] == character):
             print(character + " found at index 2")
-            count =+ 1
+            count = count + 1
 
         if (word[3] == character):
             print(character + " found at index 3")
-            count =+ 1
+            count = count + 1
 
         if (word[4] == character):
-            print(character + " found in index 4")
-            count =+ 1
+            print(character + " found at index 4")
+            count = count + 1
 
         if count == 0: 
-            print(character + " not found")
+            print("No instances of" + character + " in " + word)
+        if count > 1:
+            print(str(count) + " instances of " + character + " found in " + word)
+        else:
+#Program output
+            print(str(count) + " instance of " + character + " found in " + word)
 
-        print(str(count) + " instance of " + character + " found in " + word)
-    else: print("Error: Character must be a single character")
+#Error messages if contraints not met
+    else: 
+        print("Error: Character must be a single character")
+        quit()
 
 else:
     print("Error: Word must contain 5 characters")
+    quit()
